071e9238ae12a179d8c2390054bc1bebda6afaf8
lavanyakapoor0810 <lavanyakapoor0810@example.com>
2026-03-25 15:12:21 +0530

    Updated Shopzy code (Navbar fix + new features)

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,11 +4,15 @@ import { useNavigate } from "react-router-dom";
 export const AppContext= createContext();
 
 export const AppContextProvider = ({children}) => {
+
     const navigate=useNavigate();
     const[user, setUser] = useState(null)
     const [isSeller, setIsSeller]= useState(false)
-   
-    const value = {navigate, user, setUser, setIsSeller, isSeller }
+    const [showUserLogin, setShowUserLogin]= useState(false)
+
+
+
+    const value = {navigate, user, setUser, setIsSeller, isSeller,showUserLogin, setShowUserLogin }
 
     return <AppContext.Provider value ={value}>
         {children}
