725d3017d6cf9ba422369469ca1b1ea1bc673258
Aanvi02 <aanvirohilla26@gmail.com>
2026-03-25 17:25:57 +0530

    Added MainBanner, Categories, Home page and fixed routing/navbar issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+
 const App = () => {
+
+  const issellerPath = useLocation().pathname.includes("seller");
   return (
     <div>
-      <Navbar/>
+      {issellerPath ? null : <Navbar/>}
+      <div className={`${issellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+        <Routes>
+          <Route path = '/' element = {<Home/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
