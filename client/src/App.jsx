f7ed36c2f8ee83add8dae4d519a0574b03cf799d
Aanvi02 <aanvirohilla26@gmail.com>
2026-03-26 15:12:50 +0530

    updated product card and fixed bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <div>
       {issellerPath ? null : <Navbar/>}
+      <Toaster />
       <div className={`${issellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path = '/' element = {<Home/>}/>
