fe289b90c6b48215bb417c2e98cb55d768a6197c
lavanyakapoor0810 <lavanyakapoor0810@example.com>
2026-04-26 05:21:03 +0530

    completed my orders page

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -1,40 +1,3 @@
-// // import React, { useEffect } from 'react'
-// import React, { useEffect, useState } from 'react'
-// import { useAppContext } from '../context/AppContext'
-// import { dummyOrders } from '../assets/assets' // if using dummy data
-// const MyOrders = () => {
-//     const [myOrders, setMyOrders] = useState([])
-//     const {currency}= useAppContext()
-  
-//     const fetchMyOrders= async () =>{
-//         setMyOrders(dummyOrders)
-//     }
-
-//     useEffect(()=>{
-//         fetchMyOrders()
-//     },[])
-
-//     return (
-//   <div className='mt-16 pb-16'>
-
-//     <div className='flex flex-col items-end w-max mb-8'>
-//       <p className='text-2xl font-medium uppercase'>My orders</p>
-//       <div className='w-16 h-0.5 bg-primary rounded-full'></div>
-//     </div>
-
-//     {myOrders.map((order, index) => (
-//       <div key={index} className='border border-gray-300 rounded-lg mb-10 p-4 py-5 max-w-4xl'>
-//         <p className='flex justify-between md:items-center text-gray-400 md:font-medium max-md:flex-col'>
-//           <span>OrderId: {order._id}</span>
-//           <span>Payment: {order.paymentType}</span>
-//           <span>Total: {currency} {order.amount}</span>
-//         </p>
-//       </div>
-//     ))}
-
-//   </div>
-// )
-// export default MyOrders
 
 
 
@@ -67,10 +30,11 @@ const MyOrders = () => {
           <p className='flex justify-between md:items-center text-gray-400 md:font-medium max-md:flex-col'>
             <span>OrderId: {order._id}</span>
             <span>Payment: {order.paymentType}</span>
-            <span>Total: {currency} {order.amount}</span>
+            <span>Total Amount: {currency} {order.amount}</span>
           </p>
           {order.items.map((item, index)=>(
-            <div>
+            <div key={index}
+            className={'relative bg-white text-gray-500/70 ${order.items.length !== index+1  "order-b"} border-gray-300 flex flex-col md:flex-row md:items-center justify-between p-4 py-5 md:gap-16 w-full max-w-4xl'}>
 
                 <div className ='flex items-center mb-4 md:mb-0'>
                     <div className='bg-primary/10 p-4 rounded-lg'>
@@ -83,7 +47,7 @@ const MyOrders = () => {
                     </div>
 
 
-                <div className='text-primary text-lg font-medium' >
+                <div className='flex flex-col justify-center md:ml-8 mb-4 md:mb=0' >
                     <p>Quantity:{item.quantity || "1"}</p>
                     <p>Status:{order.status}</p>
                     <p>Date:{new Date(order.createdAt).toLocaleDateString()}</p>
