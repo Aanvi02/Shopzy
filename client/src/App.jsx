0513cd065d161dcba51b4c9d73ae8aa0b6305d75
lavanyakapoor0810 <lavanyakapoor0810@example.com>
2026-04-26 06:02:25 +0530

    seller login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,52 @@
+// import React from 'react'
+// import Navbar from './components/Navbar'
+// import { Route, Routes, useLocation } from 'react-router-dom'
+// import { Toaster } from 'react-hot-toast';
+// import Home from './pages/Home'
+// import Footer from './components/Footer'
+// import { useAppContext } from './context/AppContext';
+// import Login from './components/Login'
+// import AllProducts from './pages/AllProducts';
+// import ProductCategory from './pages/ProductCategory';
+// import ProductDetails from './pages/ProductDetails';
+// import Cart from './pages/Cart';
+// import AddAddress from './pages/AddAddress';
+// import MyOrders from './pages/MyOrders';
+
+// const App = () => {
+
+//   const issellerPath = useLocation().pathname.includes("seller");
+//   const {showUserLogin ,isSeller }= useAppContext()
+//   return (
+//     <div>
+//       {issellerPath ? null : <Navbar/>}
+//       {showUserLogin ? <Login/> : null}
+
+//       <Toaster />
+//       <div className={`${issellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+//         <Routes>
+//           <Route path = '/' element = {<Home/>}/>
+//           <Route path = '/products' element = {<AllProducts/>}/>
+//           <Route path = '/products/:category' element = {<ProductCategory/>}/>
+//           <Route path = '/products/:category/:id' element = {<ProductDetails/>}/>
+//           <Route path = '/cart' element = {<Cart/>}/>
+//           <Route path = '/add-address' element = {<AddAddress/>}/>
+//           <Route path = '/my-orders' element = {<MyOrders/>}/>
+//           <Route path = '/seller' element = {isSeller ? null : <SellerLogin/>}/>
+// </Route>
+//         </Routes>
+//       </div>
+//     {!issellerPath && <Footer/>}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
 import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
@@ -12,30 +61,32 @@ import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import AddAddress from './pages/AddAddress';
 import MyOrders from './pages/MyOrders';
-
+import SellerLogin from "./components/seller/SellerLogin";
 const App = () => {
 
   const issellerPath = useLocation().pathname.includes("seller");
-  const {showUserLogin}= useAppContext()
+  const { showUserLogin, isSeller } = useAppContext()
+
   return (
     <div>
-      {issellerPath ? null : <Navbar/>}
-      {showUserLogin ? <Login/> : null}
+      {issellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
 
       <Toaster />
       <div className={`${issellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
-          <Route path = '/' element = {<Home/>}/>
-          <Route path = '/products' element = {<AllProducts/>}/>
-          <Route path = '/products/:category' element = {<ProductCategory/>}/>
-          <Route path = '/products/:category/:id' element = {<ProductDetails/>}/>
-          <Route path = '/cart' element = {<Cart/>}/>
-          <Route path = '/add-address' element = {<AddAddress/>}/>
-          <Route path = '/my-orders' element = {<MyOrders/>}/>
-         
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<AllProducts />} />
+          <Route path='/products/:category' element={<ProductCategory />} />
+          <Route path='/products/:category/:id' element={<ProductDetails />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/add-address' element={<AddAddress />} />
+          <Route path='/my-orders' element={<MyOrders />} />
+          <Route path='/seller' element={isSeller ? null : <SellerLogin />} /> {/* ✅ fixed */}
         </Routes>
       </div>
-    {!issellerPath && <Footer/>}
+
+      {!issellerPath && <Footer />}
     </div>
   )
 }
